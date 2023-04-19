841437e4d07fb51305a618eef6eec75ba814fbdb
Papgooner <tomperkes@yahoo.co.uk>
2023-04-19 14:25:23 +0100

    login form styling

--- employeemanager/src/Components/LoginForm.js
+++ employeemanager/src/Components/LoginForm.js
@@ -1,26 +1,32 @@
 import './LoginForm.css'
 import Header from "../Components/Header";
+import { useState } from "react";
 
 function LoginForm(){
+
+    const [PopUpStyle, ShowPopUp] = useState("hide");
+
+    function PopUp(){
+        ShowPopUp("loginPopUp")
+        setTimeout(() => ShowPopUp("hide"), 3000)
+    }
     return (
         <div className="LoginScreenDiv">
             <Header headerTitle="Employee Manager" /> 
-            <form>
-            <label className="LoginLabels" htmlFor="UserNameinput">Username:</label>
+            <form className="LoginForm">
             <br></br>
-            <input type="text" placeholder="username" className="LogininputBox" id="UsernameInput" /><br></br>
+            <input type="text" placeholder="USERNAME" className="LogininputBox" id="UsernameInput" /><br></br>
             <br></br>
-            <label className="LoginLabels" htmlFor="PasswordInput">Password:</label>
             <br></br>
-            <input type="password" placeholder="password" className="LogininputBox" id="PasswordInput" />
+            <input type="password" placeholder="PASSWORD" className="LogininputBox" id="PasswordInput" />
             </form>
 
-            <div className="Login-Pri">Login</div>
+            <div className="Login-Pri" onClick={PopUp}>Login</div>
             <p className="Login-Pri-Text">Or login using</p>
 
             <div className="Login-Alt">
-                <div className="facebook"></div>
-                <div className="google"></div>
+                <div className="facebook Alt-Btn"></div>
+                <div className="google Alt-Btn"></div>
             </div>
         </div>
     )
